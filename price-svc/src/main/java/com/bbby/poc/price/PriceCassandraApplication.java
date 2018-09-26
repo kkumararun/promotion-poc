f397c5800a3394ac44f4388768a3a93e963b08b3
Ankur Jain <ankur_150889@yahoo.com>
2018-09-26 11:28:09 +0530

    Added Source Code for Pricing Module

---
+++ price-svc/src/main/java/com/bbby/poc/price/PriceCassandraApplication.java
@@ -0,0 +1,12 @@
+package com.bbby.poc.price;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceCassandraApplication.class, args);
+	}
+}
